4d923cafcd3f53b8d283150324d73f575c55eb7f
taylord109 <cisco.cat@hotmail.com>
2018-11-22 04:06:08 -0500

    Added keys

--- lib/server.ts
+++ lib/server.ts
@@ -1,6 +1,17 @@
 import app from './app'
+import * as https from 'https';
+import * as fs from 'fs';
 const PORT = 2100;
 
-app.listen(PORT, () => {
-    console.log('Express server listening on port ' + PORT);
+const httpsOptions = {
+    key: fs.readFileSync('./lib/config/key.pem'),
+    cert: fs.readFileSync('./lib/config/cert.pem')
+};
+
+https.createServer(httpsOptions, app).listen(PORT, ()=>{
+    console.log('Express server listening to port ' + PORT);
 })
+
+// app.listen(PORT, () => {
+//     console.log('Express server listening on port ' + PORT);
+// })
